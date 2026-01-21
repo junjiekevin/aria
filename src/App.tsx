663de9381d294af4ff4f4a49a0a330bf5b9c206e
Kevin Lee <leejunjiekevin@gmail.com>
2026-01-21 16:21:14 -0500

    Simplify routing: AuthPage is now the landing page
    - Route / now points to AuthPage instead of LandingPage
    - Removed /auth route (AuthPage is at /)
    - Deleted unused LandingPage component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import ProtectedRoute from './components/ProtectedRoute.tsx'
 
 const StudentFormPage = React.lazy(() => import('./pages/StudentFormPage.tsx'))
 const DashboardPage = React.lazy(() => import('./pages/DashboardPage.tsx'))
-const LandingPage = React.lazy(() => import('./pages/LandingPage.tsx'))
 const SchedulePage = React.lazy(() => import('./pages/SchedulePage.tsx'))
 const AccountPage = React.lazy(() => import('./pages/AccountPage.tsx'))
 const HelpPage = React.lazy(() => import('./pages/HelpPage.tsx'))
@@ -15,14 +14,13 @@ const App = () => {
     return (
         <React.Suspense fallback={<div>Loading...</div>}>
             <Routes>
-                <Route path="/" element={<LandingPage />} />
+                <Route path="/" element={<AuthPage />} />
                 <Route path="/form/:scheduleId" element={<StudentFormPage />} />
                 <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
                 <Route path="/schedule/:scheduleId" element={<ProtectedRoute><SchedulePage /></ProtectedRoute>} />
                 <Route path="/account" element={<ProtectedRoute><AccountPage /></ProtectedRoute>} />
                 <Route path="/help" element={<ProtectedRoute><HelpPage /></ProtectedRoute>} />
                 <Route path="/about" element={<ProtectedRoute><AboutPage /></ProtectedRoute>} />
-                <Route path="/auth" element={<AuthPage />} />
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </React.Suspense>
